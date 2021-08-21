3687aae01ccdd663f975bc4cc557f41d08574825
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-08-21 19:19:49 +0900

    Update subjectdbhandling.js

--- subjectdbhandling.js
+++ subjectdbhandling.js
@@ -10,9 +10,39 @@ exports.search = function(key, res) {
                     "error": "error"
                 })
             } else {
-                res.send({
-                    "result": results
-                })
+                if (results.length > 0) {
+                    if (results.length > 1) { //똑같은 수업 중복되는것 막기
+                        let classname = [];
+                        for (var i = 0; i < results.length; i++) {
+                            if (results[i].name.indexOf('(') != -1) { //주로 ()가 과목명에 존재함. 그 앞이 주된 과목명이기에 (를 기준으로 하였음.
+                                var temp = results[i].name.substring(0, results[i].name.indexOf('('));
+                                if (!classname.includes(temp)) {
+                                    classname.push(temp);
+                                } else //똑같은 수업이 있으면
+                                {
+                                    results.splice(i, 1);
+                                    i = i - 1;
+                                }
+                            } else {
+                                if (!classname.includes(results[i].name)) {
+                                    classname.push(results[i].name);
+                                } else {
+                                    results.splice(i, 1);
+                                    i = i - 1;
+                                }
+                            }
+                        }
+                        res.send({
+                            "result": results
+                        });
+
+
+                    } else {
+                        res.send({
+                            "result": results
+                        });
+                    }
+                }
             }
         });
 }
