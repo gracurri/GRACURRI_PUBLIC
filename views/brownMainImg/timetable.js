619d45fb4a9c4c60083aaeadb79b6e73fde09436
intersoom <sallybig@naver.com>
2021-08-29 01:26:00 +0900

    grade_sub 과목 정보 요청

--- views/brownMainImg/timetable.js
+++ views/brownMainImg/timetable.js
@@ -12,28 +12,76 @@ window.onload=function(){
     const four1 = doc.getElementById('41');
     const four2 = doc.getElementById('42');
 
+    
+    let requestSem;
+
     const sem = async()=>{
         // 데이터 get
-        fetch("http://localhost:3000/to_attend")
+        fetch("http://localhost:3000/to_attend?semester=" + requestSem)
         .then((res) => res.json())
         .then((res) => {
             console.log(res)
             if(res.code === 200){
-                const userid = res.id;
-                const grade = res.semester;
+                location.href='localhost:3000/grade_sub';
+                let semester = doc.getElementById('semester');
+                semester.innerText = grade + "의"
+                //res.result는 서버 응답 맞춰서 수정해야함
+                const countSubject = Object.keys(res.result).length
+                const bread = doc.querySelector('.bread')
+
+                for(i = 0; i < countSubject; i++){
+                    const subjectDiv = doc.createElement('div');
+                    const subjectP = doc.createElement('p');
+                    //name은 나중애 서버 응답 맞춰서 수정해야함
+                    const subjectName = doc.createTextNode(res.result[i].name)
+
+                    subjectP.appendChild(subjectName)
+                    subjectP.classList.add('subject')
+
+                    subjectDiv.appendChild(subjectP)
+                    subjectDiv.classList.add('list')
+
+                    subjectResult.appendChild(bread);
+                }
             }
         })
         .catch(err => {
             console.log('Fetch Error', err);
         });
+
+        
     }
 
-    one1.addEventListener('click', sem);
-    one2.addEventListener('click', sem);
-    two1.addEventListener('click', sem);
-    two2.addEventListener('click', sem);
-    three1.addEventListener('click', sem);
-    three2.addEventListener('click', sem);
-    four1.addEventListener('click', sem);
-    four2.addEventListener('click', sem);
+    one1.addEventListener('click', function(){
+        requestSem = 'one';
+        sem();
+    });
+    one2.addEventListener('click', function(){
+        requestSem = 'two';
+        sem();
+    });
+    two1.addEventListener('click', function(){
+        requestSem = 'three';
+        sem();
+    });
+    two2.addEventListener('click', function(){
+        requestSem = 'four';
+        sem();
+    });
+    three1.addEventListener('click', function(){
+        requestSem = 'five';
+        sem();
+    });
+    three2.addEventListener('click', function(){
+        requestSem = 'six';
+        sem();
+    });
+    four1.addEventListener('click', function(){
+        requestSem = 'seven';
+        sem();
+    });
+    four2.addEventListener('click', function(){
+        requestSem = 'eight';
+        sem();
+    });
 };
