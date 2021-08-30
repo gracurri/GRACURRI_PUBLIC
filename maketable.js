135c69c3652070e48f0fc68f802c3153a4ee796d
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-08-30 17:55:03 +0900

    시간표 짜기 중

--- maketable.js
+++ maketable.js
@@ -34,11 +34,37 @@ exports.makeplan = function(req, res) { //학기들 계획짜는 함수
     var attended_names = []
     db.query('USE subjects;');
     let user = storestatus(req);
+    db.query('use gracurri_user;');
+    db.query('SELECT major from users where EMAIL=?', [req.body.id], function(error, results) {
+        if (results.length > 0) {
+            user.major = results[0].major;
+        }
+    });
     for (var i = 0; i < user.attended.length; i++) {
         db.query('SELECT name from subject where id=?', [user.attended[i]], function(error, results, fields) {
-            attended_names.push(results[0]);
+            attended_names.push(results[0].name);
         });
     } //들은 과목들 이름을 불러옴(들은과목 넣어주는 것 방지위해)
+    let currsem = user.semester + 1; //알고리즘 진행과정에서의 현재 학기
+    while (currsem < 9) {
+        var max = 18;
+        var major = 0;
+        var etc = 0;
+        if (currsem / 2 <= 1) { //1학년때
+            var max = 22;
+            let major = 9; //1학년전기,전필
+            let etc = 8; //1학년교필
+            if (currsem % 2 == 1) { //1학기
+                db.query('SELECT id,name from subject')
+            }
+        } else if (currsem / 2 <= 4) { //2학년
+
+        } else if (currsem / 2 <= 6) { //3학년
+
+        } else if (currsem / 2 <= 8) { //4학년
+
+        }
+    }
 }
 class userstatus {
     current = 0; //현재 이수학점
@@ -54,48 +80,103 @@ class userstatus {
     christion = 0; //기독교과목
     semester = 0;
     attended = [];
+    major = '';
     constructor(attended) {
         this.attended = attended;
         db.query('USE subjects;');
         for (var i = 0; i < length(attended); i++) {
-            db.query('SELECT division,unit,etc_div,targetstudent from subject where id=?', [attended[i]], function(error, results, fields) {
-                current += parseInt(results[0].unit);
-                //전공 및 교양 필수/선택 체크
-                if (results[0].division.includes("전기")) {
-                    current_major_basic += results[0].unit;
-                } else if (results[0].division.includes("전필")) {
-                    current_major_must += results[0].unit;
-                } else if (results[0].division.includes("전선")) {
-                    curr_major_select += results[0].unit;
-                } else if (results[0].division.includes("교필")) {
-                    curr_etc_must += results[0].unit;
-                } else if (results[0].division.includes("교선")) {
-                    //교선 분야 확인
-                    if (results[0].etc_div.includes('사회과학')) {
-                        this.socialstudy = 1
-                    } else if (results[0].etc_div.includes('국제어문')) {
-                        this.language = 1;
-                    } else if (results[0].etc_iv.inclues('인문학')) {
-                        this.humanities = 1;
-                    } else if (results[0].etc_idv.includes('숭실품성')) {
-                        this.ethics = 1;
-                    }
-                    curr_etc_select += results[0].unit;
-                }
-                if (results[0].targetstudent.includes('1학년')) {
-                    if (this.semester < 1) {
-                        this.semester = 1;
-                    }
-                } else if (results[0].targetstudent.includes('2학년')) {
-                    if (this.semester < 2) {
-                        this.semester = 2;
-                    }
-                } else if (results[0].targetstudent.includes('3학년')) {
-                    if (this.semester < 3) {
-                        this.semester = 3;
-                    }
-                }
-            })
+            var search = false;
+            //2학기과목
+            db.query('SELECT division,unit,etc_div,targetstudent from subject where id=?', [attended[i]],
+                    function(error, results, fields) {
+                        if (results.length > 0) {
+                            current += parseInt(results[0].unit);
+                            //전공 및 교양 필수/선택 체크
+                            if (results[0].division.includes("전기")) {
+                                current_major_basic += results[0].unit;
+                            } else if (results[0].division.includes("전필")) {
+                                current_major_must += results[0].unit;
+                            } else if (results[0].division.includes("전선")) {
+                                curr_major_select += results[0].unit;
+                            } else if (results[0].division.includes("교필")) {
+                                curr_etc_must += results[0].unit;
+                            } else if (results[0].division.includes("교선")) {
+                                //교선 분야 확인
+                                if (results[0].etc_div.includes('사회과학')) {
+                                    this.socialstudy = 1
+                                } else if (results[0].etc_div.includes('국제어문')) {
+                                    this.language = 1;
+                                } else if (results[0].etc_iv.inclues('인문학')) {
+                                    this.humanities = 1;
+                                } else if (results[0].etc_idv.includes('숭실품성')) {
+                                    this.ethics = 1;
+                                }
+                                curr_etc_select += results[0].unit;
+                            }
+                            if (results[0].targetstudent.includes('1학년')) {
+                                if (this.semester < 2) {
+                                    this.semester = 2;
+                                }
+                            } else if (results[0].targetstudent.includes('2학년')) {
+                                if (this.semester < 4) {
+                                    this.semester = 4;
+                                }
+                            } else if (results[0].targetstudent.includes('3학년')) {
+                                if (this.semester < 6) {
+                                    this.semester = 6;
+                                }
+                            }
+                        }
+                        search = true;
+                    })
+                //1학기과목
+            if (search == false) {
+                db.query('SELECT division,unit,etc_div,targetstudent from subject_1 where id=?', [attended[i]],
+                    function(error, results, fields) {
+                        if (results.length > 0) {
+                            current += parseInt(results[0].unit);
+                            //전공 및 교양 필수/선택 체크
+                            if (results[0].division.includes("전기")) {
+                                current_major_basic += results[0].unit;
+                            } else if (results[0].division.includes("전필")) {
+                                current_major_must += results[0].unit;
+                            } else if (results[0].division.includes("전선")) {
+                                curr_major_select += results[0].unit;
+                            } else if (results[0].division.includes("교필")) {
+                                curr_etc_must += results[0].unit;
+                            } else if (results[0].division.includes("교선")) {
+                                //교선 분야 확인
+                                if (results[0].etc_div.includes('사회과학')) {
+                                    this.socialstudy = 1
+                                } else if (results[0].etc_div.includes('국제어문')) {
+                                    this.language = 1;
+                                } else if (results[0].etc_iv.inclues('인문학')) {
+                                    this.humanities = 1;
+                                } else if (results[0].etc_idv.includes('숭실품성')) {
+                                    this.ethics = 1;
+                                }
+                                curr_etc_select += results[0].unit;
+                            }
+                            if (results[0].targetstudent.includes('1학년')) {
+                                if (this.semester < 1) {
+                                    this.semester = 1;
+                                }
+                            } else if (results[0].targetstudent.includes('2학년')) {
+                                if (this.semester < 3) {
+                                    this.semester = 3;
+                                }
+                            } else if (results[0].targetstudent.includes('3학년')) {
+                                if (this.semester < 5) {
+                                    this.semester = 5;
+                                }
+                            }
+                        } else {
+                            console.log("error occurred");
+                        }
+
+                    })
+            }
+
         }
 
     }
--- subjectdbhandling.js
+++ subjectdbhandling.js
@@ -3,12 +3,14 @@ exports.search = function(key, res) { //info_input과목검색
     console.log(key);
     subjectdb.query('use subjects;')
     var error = 0
+    var search = false;
     subjectdb.query('SELECT name,id from subject WHERE name LIKE' + subjectdb.escape('%' + key + '%') + ';',
         function(error, results, fields) {
             if (error) {
                 error = 1;
             } else {
                 if (results.length > 0) {
+                    search = true;
                     if (results.length > 1) { //똑같은 수업 중복되는것 막기
                         let classname = [];
                         for (var i = 0; i < results.length; i++) {
@@ -43,50 +45,59 @@ exports.search = function(key, res) { //info_input과목검색
                 }
             }
         });
-    subjectdb.query('SELECT name,id from subject_1 WHERE name LIKE' + subjectdb.escape('%' + key + '%') + ';',
-        function(error, results, fields) {
-            if (error) {
-                error = 1;
-            } else {
-                if (results.length > 0) {
-                    if (results.length > 1) { //똑같은 수업 중복되는것 막기
-                        let classname = [];
-                        for (var i = 0; i < results.length; i++) {
-                            if (results[i].name.indexOf('(') != -1) { //주로 ()가 과목명에 존재함. 그 앞이 주된 과목명이기에 (를 기준으로 하였음.
-                                var temp = results[i].name.substring(0, results[i].name.indexOf('('));
-                                if (!classname.includes(temp)) {
-                                    classname.push(temp);
-                                } else //똑같은 수업이 있으면
-                                {
-                                    results.splice(i, 1);
-                                    i = i - 1;
-                                }
-                            } else {
-                                if (!classname.includes(results[i].name)) {
-                                    classname.push(results[i].name);
+    if (search === false) {
+        subjectdb.query('SELECT name,id from subject_1 WHERE name LIKE' + subjectdb.escape('%' + key + '%') + ';',
+            function(error, results, fields) {
+                if (error) {
+                    error = 1;
+                } else {
+                    if (results.length > 0) {
+                        search = true;
+                        if (results.length > 1) { //똑같은 수업 중복되는것 막기
+                            let classname = [];
+                            for (var i = 0; i < results.length; i++) {
+                                if (results[i].name.indexOf('(') != -1) { //주로 ()가 과목명에 존재함. 그 앞이 주된 과목명이기에 (를 기준으로 하였음.
+                                    var temp = results[i].name.substring(0, results[i].name.indexOf('('));
+                                    if (!classname.includes(temp)) {
+                                        classname.push(temp);
+                                    } else //똑같은 수업이 있으면
+                                    {
+                                        results.splice(i, 1);
+                                        i = i - 1;
+                                    }
                                 } else {
-                                    results.splice(i, 1);
-                                    i = i - 1;
+                                    if (!classname.includes(results[i].name)) {
+                                        classname.push(results[i].name);
+                                    } else {
+                                        results.splice(i, 1);
+                                        i = i - 1;
+                                    }
                                 }
                             }
+                            res.send({
+                                "result": results
+                            });
+
+
+                        } else {
+                            res.send({
+                                "result": results
+                            });
                         }
-                        res.send({
-                            "result": results
-                        });
-
-
-                    } else {
-                        res.send({
-                            "result": results
-                        });
                     }
                 }
-            }
-        });
+            });
+    }
     if (error == 1) {
         res.send({
             "code": 400,
             "error": "error in search"
         });
+    }
+    if (search === false) {
+        res.send({
+            "code": 200,
+            "result": "검색어에 해당하는 과목이 없습니다.";
+        })
     }
 }
