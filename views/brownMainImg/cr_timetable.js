ac6ee618d2f15f882bc2a4ebb0df31f610a627ac
SuJin <1127sujink@naver.com>
2021-09-03 20:53:10 +0900

    timetable js

--- views/brownMainImg/cr_timetable.js
+++ views/brownMainImg/cr_timetable.js
@@ -1,51 +1,25 @@
-let table = document.createElement('table');
-let thead = document.createElement('thead');
-let tbody = document.createElement('tbody');
+//회원가입 함수
+window.onload = function(){
 
-table.appendChild(thead);
-table.appendChild(tbody);
+    var doc = document;
+    const make = doc.getElementById('maketable');
 
-// Adding the entire table to the body tag
-document.getElementById('body').appendChild(table);
-
-let row_1 = document.createElement('tr');
-let heading_1 = document.createElement('th');
-heading_1.innerHTML = "";
-let heading_2 = document.createElement('th');
-heading_2.innerHTML = "월";
-let heading_3 = document.createElement('th');
-heading_3.innerHTML = "화";
-
-row_1.appendChild(heading_1);
-row_1.appendChild(heading_2);
-row_1.appendChild(heading_3);
-thead.appendChild(row_1);
-
-// Creating and adding data to second row of the table
-let row_2 = document.createElement('tr');
-let row_2_data_1 = document.createElement('td');
-row_2_data_1.innerHTML = "1.";
-let row_2_data_2 = document.createElement('td');
-row_2_data_2.innerHTML = "James Clerk";
-let row_2_data_3 = document.createElement('td');
-row_2_data_3.innerHTML = "Netflix";
-
-row_2.appendChild(row_2_data_1);
-row_2.appendChild(row_2_data_2);
-row_2.appendChild(row_2_data_3);
-tbody.appendChild(row_2);
+    const sem = async()=>{
 
 
-// Creating and adding data to third row of the table
-let row_3 = document.createElement('tr');
-let row_3_data_1 = document.createElement('td');
-row_3_data_1.innerHTML = "2.";
-let row_3_data_2 = document.createElement('td');
-row_3_data_2.innerHTML = "Adam White";
-let row_3_data_3 = document.createElement('td');
-row_3_data_3.innerHTML = "Microsoft";
 
-row_3.appendChild(row_3_data_1);
-row_3.appendChild(row_3_data_2);
-row_3.appendChild(row_3_data_3);
-tbody.appendChild(row_3);
+        fetch("http://localhost:3000/timetable")
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res)
+            if(res.code === ){
+
+            }
+        })
+        .catch(err => {
+            console.log('Fetch Error', err);
+        });
+    }
+}
+
+}
