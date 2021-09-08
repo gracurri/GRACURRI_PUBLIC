f7963780fbcfe4784fe13174c30490a8460230f6
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-09-09 00:06:22 +0900

    Update classesdbhandling.js

--- classesdbhandling.js
+++ classesdbhandling.js
@@ -27,21 +27,21 @@ var semeseterset = function(semester, results) {
         let sem = semester;
         let codestring = '';
         if (semester === "one") {
-            codestring = results.one;
+            codestring = results[0].one;
         } else if (semester === 'two') {
-            codestring = results.two;
+            codestring = results[0].two;
         } else if (semester === 'three') {
-            codestring = results.three;
+            codestring = results[0].three;
         } else if (semester === 'four') {
-            codestring = results.four;
+            codestring = results[0].four;
         } else if (semester === 'five') {
-            codestring = results.five;
+            codestring = results[0].five;
         } else if (semester === 'six') {
-            codestring = results.six;
+            codestring = results[0].six;
         } else if (semester === 'seven') {
-            codestring = results.seven;
+            codestring = results[0].seven;
         } else {
-            codestring = results.eight;
+            codestring = results[0].eight;
         }
         resolve(codestring)
     })
@@ -74,35 +74,28 @@ exports.gettoattend = function(query, res) {
                                         })
                                 }
                             });
-                            
+
                     }
-                    let semreturn=''
-                            if(query.semester==='one'){
-                                semreturn='1-1';
-                            }
-                            else if(query.semester==='two'){
-                                semreturn='1-2';
-                            }
-                            else if(query.semester==='three'){
-                                semreturn='2-1';
-                            }
-                            else if(query.semester==='four'){
-                                semreturn='2-2';
-                            }
-                            else if(query.semester==='five'){
-                                semreturn='3-1';
-                            }
-                            else if(query.semester==='six'){
-                                semreturn='3-2';
-                            }
-                            else if(query.semester==='seven'){
-                                semreturn='3-1';
-                            }
-                            else if(query.semester==='eight'){
-                                semreturn='3-2';
-                            }
-                    return (names,semreturn);
-                }).then(function(names,semreturn) {
+                    let semreturn = ''
+                    if (query.semester === 'one') {
+                        semreturn = '1-1';
+                    } else if (query.semester === 'two') {
+                        semreturn = '1-2';
+                    } else if (query.semester === 'three') {
+                        semreturn = '2-1';
+                    } else if (query.semester === 'four') {
+                        semreturn = '2-2';
+                    } else if (query.semester === 'five') {
+                        semreturn = '3-1';
+                    } else if (query.semester === 'six') {
+                        semreturn = '3-2';
+                    } else if (query.semester === 'seven') {
+                        semreturn = '3-1';
+                    } else if (query.semester === 'eight') {
+                        semreturn = '3-2';
+                    }
+                    return (names, semreturn);
+                }).then(function(names, semreturn) {
                     res.send({
                         "code": 200,
                         "result": names,
@@ -113,61 +106,61 @@ exports.gettoattend = function(query, res) {
             }
         })
 }
-exports.tableshowsem=function(query,res){
-    return new Promise(function(resolve,reject){
+exports.tableshowsem = function(query, res) {
+    return new Promise(function(resolve, reject) {
         db.query('use gracurri_user;');
-        db.query('SELECT semester from users WHERE EMAIL=?',[query.email],
-        function(error,results,fields){
-            currsem=results[0].semester;
-            resolve(currsem,query.email);
-        }); 
+        db.query('SELECT semester from users WHERE EMAIL=?', [query.email],
+            function(error, results, fields) {
+                currsem = results[0].semester;
+                resolve(currsem, query.email);
+            });
     })
-   
+
 }
-exports.getclasses=function(currsem,email){
-    return new Promise(function(resolve,reject){
+exports.getclasses = function(currsem, email) {
+    return new Promise(function(resolve, reject) {
         db.query('use gracurri_user;');
         db.query('SELECT ? from semesters where EMAIL=?', [query.semester, query.email],
-        function(error, results, fields) {
-            if (error) {
-                res.send({
-                    "code": 400,
-                    "result": "error!"
-                })
-            } else {
-                semeseterset(query.semester, results).then(function(code) {
-                    var names = [];
-                    var timeandloc=[];
-                    for (var i = 0; i < code.length; i += 10) {
-                        var temp = code.slice(i, i + 10);
-                        db.query('USE subjects;');
-                        db.query('SELECT name,classtimeandlocation from subject where id=?', [temp],
-                            function(error, result) {
-                                if (result.length > 0) {
-                                    names.push(result[0].name);
-                                    timeandloc.push(result[0].classtimeandlocation);
-                                } else {
-                                    db.query('SELECT name,classtimeandlocation from subject_1 where id=?', [temp],
-                                        function(errors, results) {
-                                            if (!errors && results.length > 0) {
-                                                names.push(results[0].name);
-                                                timeandloc.push(results[0].classtimeandlocation);
-                                            }
-                                        })
-                                }
-                            });
-                            
-                    }
-                    return (names,timeandloc);
-                }).then(function(names,timeandloc) {
+            function(error, results, fields) {
+                if (error) {
                     res.send({
-                        "code": 200,
-                        "result": names,
-                        "timeandloc": timeandloc
+                        "code": 400,
+                        "result": "error!"
                     })
-                });
+                } else {
+                    semeseterset(query.semester, results).then(function(code) {
+                        var names = [];
+                        var timeandloc = [];
+                        for (var i = 0; i < code.length; i += 10) {
+                            var temp = code.slice(i, i + 10);
+                            db.query('USE subjects;');
+                            db.query('SELECT name,classtimeandlocation from subject where id=?', [temp],
+                                function(error, result) {
+                                    if (result.length > 0) {
+                                        names.push(result[0].name);
+                                        timeandloc.push(result[0].classtimeandlocation);
+                                    } else {
+                                        db.query('SELECT name,classtimeandlocation from subject_1 where id=?', [temp],
+                                            function(errors, results) {
+                                                if (!errors && results.length > 0) {
+                                                    names.push(results[0].name);
+                                                    timeandloc.push(results[0].classtimeandlocation);
+                                                }
+                                            })
+                                    }
+                                });
 
-            }
-        })
+                        }
+                        return (names, timeandloc);
+                    }).then(function(names, timeandloc) {
+                        res.send({
+                            "code": 200,
+                            "result": names,
+                            "timeandloc": timeandloc
+                        })
+                    });
+
+                }
+            })
     })
 }
