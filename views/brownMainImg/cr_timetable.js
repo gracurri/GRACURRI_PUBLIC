644cfdb1f9670954ebfae66e8d7f7c7619274163
SuJin <1127sujink@naver.com>
2021-09-10 02:59:58 +0900

    조건 추가

--- views/brownMainImg/cr_timetable.js
+++ views/brownMainImg/cr_timetable.js
@@ -9,21 +9,6 @@ window.onload = function(){
         return value? value[2] : null;
     };  
 
-    
-
-    // function mTalbe(data){
-    //     var table = doc.getElementById('target');
-
-    //     for(var i = 0; i < data.length; i++){
-    //         var row = '<tr> <td>'
-    //         row += i;
-    //         row += '<td>';
-    //         row += '<td>$(data[i].name</td> </tr>'
-
-    //         table.innerHTML += row;
-    //     }
-    // }
-
     function timetonum(str){    // 시간 교시로 바꾸는 함수
         switch(str){
             case "09:00-10:15":
@@ -58,7 +43,6 @@ window.onload = function(){
     }
     
     function daytime(arr){
-        var day1 = arr[0];
         if(arr[1] == "월" || arr[1] == "화" || arr[1] == "수" || arr[1] == "목" || arr[1] == "금"){     // 일주일에 2번인 수업
             arr[2] = timetonum(arr[2]);
             arr.push('two');
@@ -76,9 +60,6 @@ window.onload = function(){
         return arr;
     }
 
-    
-    
-
     const sem = async()=>{
         let userCookieId = getCookie('userid');
         console.log(userCookieId)
@@ -92,7 +73,7 @@ window.onload = function(){
                 const countSubject = Object.keys(res.result).length;
                 var talarr = res.timeandloc;
 
-                let Montd = doc.createElement('td');
+                let Montd = doc.createElement('TD');
                 let MonDiv = doc.createElement('div');
                 let MonP = doc.createElement('p');
 
@@ -112,73 +93,91 @@ window.onload = function(){
                 let FriDiv = doc.createElement('div');
                 let FriP = doc.createElement('p');
 
-                Montd = doc.createElement('td');
+                Montd = doc.createElement('TD');
 
                 for(i = 0; i < countSubject; i++){
 
                     var arr = talarr[i].split(' ');
                     arr = daytime(arr); 
 
-                    // 일주일에 한번, 1교시
-                    if(arr.indexof('one1') != -1){
-                        if(arr.indexof('월') != -1){
-                            MonDiv = doc.createElement('div');
-                            MonP = doc.createElement('p');
-                            
-                            MonP.appendChild(res.result[i]);
-                            MonDiv.style.position = absolute;
-                            MonDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
-                            MonDiv.style.zIndex = 10;
+                    // indexOf(" ")로 하면 일주일에 2번인것도 포함됨
+                    if(arr.indexOf('월') != -1){
+                        MonDiv = doc.createElement('div');
+                        MonP = doc.createElement('p');
+                        
+                        MonP.appendChild(res.result[i]);
+                        MonDiv.style.position = absolute;
+                        MonDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
 
-                            MonDiv.appendChild(MonP);
+                        if(arr.indexOf('one2') != -1){  // 2교시 이면
+                            MonDiv.style.height = 100 + 'px';
                         }
-                        if(arr.indexof('화') != -1){
-                            TueDiv = doc.createElement('div');
-                            TueP = doc.createElement('p');
-                            
-                            TueP.appendChild(res.result[i]);
-                            TueDiv.style.position = absolute;
-                            TueDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
-                            TueDiv.style.zIndex = 10;
+                        MonDiv.style.zIndex = 10;
 
-                            TueDiv.appendChild(Tue);
-                        }
-                        if(arr.indexof('수') != -1){
-                            WedDiv = doc.createElement('div');
-                            WedP = doc.createElement('p');
-                            
-                            WedP.appendChild(res.result[i]);
-                            WedDiv.style.position = absolute;
-                            WedDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
-                            WedDiv.style.zIndex = 10;
-
-                            WedDiv.appendChild(WedP);
-                        }
-                        if(arr.indexof('목') != -1){
-                            ThuDiv = doc.createElement('div');
-                            ThuP = doc.createElement('p');
-                            
-                            ThuP.appendChild(res.result[i]);
-                            ThuDiv.style.position = absolute;
-                            ThuDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
-                            ThuDiv.style.zIndex = 10;
-
-                            ThuDiv.appendChild(ThuP);
-                        }
-                        if(arr.indexof('금') != -1){
-                            FriDiv = doc.createElement('div');
-                            FriP = doc.createElement('p');
-                            
-                            FriP.appendChild(res.result[i]);
-                            FriDiv.style.position = absolute;
-                            FriDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
-                            FriDiv.style.zIndex = 10;
-
-                            FriDiv.appendChild(FriP);
-                        }
+                        MonDiv.appendChild(MonP);
                     }
+                    if(arr.indexOf('화') != -1){
+                        TueDiv = doc.createElement('div');
+                        TueP = doc.createElement('p');
+                        
+                        TueP.appendChild(res.result[i]);
+                        TueDiv.style.position = absolute;
+                        TueDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
 
-                }
+                        if(arr.indexOf('one2') != -1){  // 2교시 이면
+                            TueDiv.style.height = 100 + 'px';
+                        }
+                        TueDiv.style.zIndex = 10;
+
+                        TueDiv.appendChild(Tue);
+                    }
+                    if(arr.indexOf('수') != -1){
+                        WedDiv = doc.createElement('div');
+                        WedP = doc.createElement('p');
+                        
+                        WedP.appendChild(res.result[i]);
+                        WedDiv.style.position = absolute;
+                        WedDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
+
+                        if(arr.indexOf('one2') != -1){  // 2교시 이면
+                            WedDiv.style.height = 100 + 'px';
+                        }
+
+                        WedDiv.style.zIndex = 10;
+
+                        WedDiv.appendChild(WedP);
+                    }
+                    if(arr.indexOf('목') != -1){
+                        ThuDiv = doc.createElement('div');
+                        ThuP = doc.createElement('p');
+                        
+                        ThuP.appendChild(res.result[i]);
+                        ThuDiv.style.position = absolute;
+                        ThuDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
+
+                        if(arr.indexOf('one2') != -1){  // 2교시 이면
+                            ThuDiv.style.height = 100 + 'px';
+                        }
+                        ThuDiv.style.zIndex = 10;
+
+                        ThuDiv.appendChild(ThuP);
+                    }
+                    if(arr.indexOf('금') != -1){
+                        FriDiv = doc.createElement('div');
+                        FriP = doc.createElement('p');
+                        
+                        FriP.appendChild(res.result[i]);
+                        FriDiv.style.position = absolute;
+                        FriDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
+
+                        if(arr.indexOf('one2') != -1){  // 2교시 이면
+                            FriDiv.style.height = 100 + 'px';
+                        }
+                        FriDiv.style.zIndex = 10;
+
+                        FriDiv.appendChild(FriP);
+                    }
+            }
 
             }
         })
