f6d84dd55f8445ecaec32e6bae63719e9a2bfa68
SuJin <1127sujink@naver.com>
2021-09-09 20:07:23 +0900

    update timetable
    일주일에 한번 1교시만 하는거

--- views/brownMainImg/cr_timetable.js
+++ views/brownMainImg/cr_timetable.js
@@ -11,18 +11,18 @@ window.onload = function(){
 
     let userCookieId = getCookie('userid');
 
-    function mTalbe(data){
-        var table = doc.getElementById('target');
+    // function mTalbe(data){
+    //     var table = doc.getElementById('target');
 
-        for(var i = 0; i < data.length; i++){
-            var row = '<tr> <td>'
-            row += i;
-            row += '<td>';
-            row += '<td>$(data[i].name</td> </tr>'
+    //     for(var i = 0; i < data.length; i++){
+    //         var row = '<tr> <td>'
+    //         row += i;
+    //         row += '<td>';
+    //         row += '<td>$(data[i].name</td> </tr>'
 
-            table.innerHTML += row;
-        }
-    }
+    //         table.innerHTML += row;
+    //     }
+    // }
 
     function timetonum(str){    // 시간 교시로 바꾸는 함수
         switch(str){
@@ -53,18 +53,27 @@ window.onload = function(){
             case "21:00-22:15":
                 str = 9;
                 break;
+        }
+        return str;
     }
     
     function daytime(arr){
         var day1 = arr[0];
         if(arr[1] == "월" || arr[1] == "화" || arr[1] == "수" || arr[1] == "목" || arr[1] == "금"){     // 일주일에 2번인 수업
             timetonum(arr[2]);
+            arr.push('two');
         }
         else{   // 바로 숫자 나오면 일주일에 한번인 수업, str = 시간
             timetonum(arr[1]);  // 교시로 바꿔주기
             if(arr[3] == arr[0]){     // 1교시 이상인 수업
                 arr[3] = arr[1] + 1;
+                arr.push('one2');
             }
+            else{   // 1교시만 하는 수업
+                arr.push('one1');
+            }
+        }
+        return arr;
     }
     
 
@@ -78,40 +87,91 @@ window.onload = function(){
                 location.assign('http://localhost:3000/cr_timetable');
                 const countSubject = Object.keys(res.result).length;
                 var talarr = res.timeandloc;
-                const time = doc.querySelector('.time');
+
+                let Montd = doc.createElement('td');
+                let MonDiv = doc.createElement('div');
+                let MonP = doc.createElement('p');
+
+                let Tuetd = doc.createElement('td');
+                let TueDiv = doc.createElement('div');
+                let TueP = doc.createElement('p');
+
+                let Wedtd = doc.createElement('td');
+                let WedDiv = doc.createElement('div');
+                let WedP = doc.createElement('p');
+
+                let Thutd = doc.createElement('td');
+                let ThuDiv = doc.createElement('div');
+                let ThuP = doc.createElement('p');
+
+                let Fritd = doc.createElement('td');
+                let FriDiv = doc.createElement('div');
+                let FriP = doc.createElement('p');
+
+                Montd = doc.createElement('td');
 
                 for(i = 0; i < countSubject; i++){
 
                     var arr = talarr[i].split(' ');
+                    arr = daytime(arr); 
 
-                    const subjectTr = doc.createElement('tr');
-                    const subjectTd = doc.createElement('td');
-                    const subjectTh = doc.createElement('th');
-                    var day;
-                    switch(day){
-                        case "월":
-                            subjectTd.appendChild(res.result[i].name);
-                            subjectTd.appendChild("<td></td><td></td><td></td><td></td>");
-                            break;
-                        case "화":
-                            subjectTd.appendChild("<td></td>");
-                            subjectTd.appendChild(res.result[i].name);
-                            subjectTd.appendChild("<td></td><td></td><td></td>");
-                            break;
-                        case "수":
-                            subjectTd.appendChild("<td></td><td></td>");
-                            subjectTd.appendChild(res.result[i].name);
-                            subjectTd.appendChild("<td></td><td></td>");
-                            break;
-                        case "목":
-                            subjectTd.appendChild("<td></td><td></td><td></td>");
-                            subjectTd.appendChild(res.result[i].name);
-                            subjectTd.appendChild("<td></td>");
-                            break;
-                        case "금":
-                            subjectTd.appendChild("<td></td><td></td><td></td><td></td>");
-                            subjectTd.appendChild(res.result[i].name);
-                            break;
+                    // 일주일에 한번, 1교시
+                    if(arr.indexof('one1') != -1){
+                        if(arr.indexof('월') != -1){
+                            MonDiv = doc.createElement('div');
+                            MonP = doc.createElement('p');
+                            
+                            MonP.appendChild(res.result[i]);
+                            MonDiv.style.position = absolute;
+                            MonDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
+                            MonDiv.style.zIndex = 10;
+
+                            MonDiv.appendChild(MonP);
+                        }
+                        if(arr.indexof('화') != -1){
+                            TueDiv = doc.createElement('div');
+                            TueP = doc.createElement('p');
+                            
+                            TueP.appendChild(res.result[i]);
+                            TueDiv.style.position = absolute;
+                            TueDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
+                            TueDiv.style.zIndex = 10;
+
+                            TueDiv.appendChild(Tue);
+                        }
+                        if(arr.indexof('수') != -1){
+                            WedDiv = doc.createElement('div');
+                            WedP = doc.createElement('p');
+                            
+                            WedP.appendChild(res.result[i]);
+                            WedDiv.style.position = absolute;
+                            WedDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
+                            WedDiv.style.zIndex = 10;
+
+                            WedDiv.appendChild(WedP);
+                        }
+                        if(arr.indexof('목') != -1){
+                            ThuDiv = doc.createElement('div');
+                            ThuP = doc.createElement('p');
+                            
+                            ThuP.appendChild(res.result[i]);
+                            ThuDiv.style.position = absolute;
+                            ThuDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
+                            ThuDiv.style.zIndex = 10;
+
+                            ThuDiv.appendChild(ThuP);
+                        }
+                        if(arr.indexof('금') != -1){
+                            FriDiv = doc.createElement('div');
+                            FriP = doc.createElement('p');
+                            
+                            FriP.appendChild(res.result[i]);
+                            FriDiv.style.position = absolute;
+                            FriDiv.style.top = (110 + 60*(arr[1] - 1))+'px';
+                            FriDiv.style.zIndex = 10;
+
+                            FriDiv.appendChild(FriP);
+                        }
                     }
 
                 }
